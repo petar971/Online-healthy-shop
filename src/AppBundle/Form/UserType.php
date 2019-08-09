ec7fdef97115aaf77d75f1d270bfa7ad880974ce
petar971 <petarkovachev9709@gmail.com>
2019-08-09 17:53:38 +0300

    user-roles

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName',TextType::class)
+            ->add('lastName',TextType::class)
+            ->add('email',TextType::class)
+            ->add('password',TextType::class)
+            ;
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+
+}
